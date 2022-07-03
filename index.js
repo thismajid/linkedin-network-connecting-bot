8c876ab4049fa69680c3209d9065a5090762777a
thismajid <mkhorshidian72@gmail.com>
2022-07-03 20:42:04 +0430

    open linkedin login page

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ["--window-size=1920,1080"],
+  });
+  const page = await browser.newPage();
+  await page.goto("https://www.linkedin.com/login", {
+    waitUntil: "networkidle0",
+  });
+})();
